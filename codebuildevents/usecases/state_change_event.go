31951c19fffee68d9a95de53fdae11222633c063
Hidetake Iwata <int128@gmail.com>
2020-10-01 10:53:42 +0900

    Refactor: state change event

--- codebuildevents/usecases/state_change_event.go
+++ codebuildevents/usecases/state_change_event.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/google/go-github/v32/github"
@@ -11,23 +12,56 @@ import (
 )
 
 func stateChangeEvent(ctx context.Context, e events.CodeBuildEvent) error {
-	commitID := findEnvironmentVariable(e, "GITHUB_WEBHOOK_HEADCOMMIT_ID")
-	if commitID == "" {
-		return fmt.Errorf("could not find the commit id")
+	s, err := calculateStatus(e)
+	if err != nil {
+		return fmt.Errorf("could not calculate the commit status: %w", err)
 	}
 
 	ghc := github.NewClient(oauth2.NewClient(ctx, oauth2.StaticTokenSource(&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")})))
-	_, _, err := ghc.Repositories.CreateStatus(ctx,
-		"int128", "codebuild-runner", commitID,
-		&github.RepoStatus{
-			Context:     github.String("CodeBuild/example"),
-			State:       github.String(determineCommitStatus(e)),
-			Description: github.String(string(e.Detail.BuildStatus)),
-		})
-	if err != nil {
+	if _, _, err := ghc.Repositories.CreateStatus(ctx, s.owner, s.repo, s.commit, &s.repoStatus); err != nil {
 		return fmt.Errorf("could not create a commit status: %w", err)
 	}
 	return nil
+}
+
+type commitStatus struct {
+	owner      string
+	repo       string
+	commit     string
+	repoStatus github.RepoStatus
+}
+
+func calculateStatus(e events.CodeBuildEvent) (*commitStatus, error) {
+	commitID := findEnvironmentVariable(e, "GITHUB_WEBHOOK_HEADCOMMIT_ID")
+	if commitID == "" {
+		return nil, fmt.Errorf("could not find the commit id")
+	}
+	owner, repo, err := parseGitHubURL(e.Detail.AdditionalInformation.Source.Location)
+	if err != nil {
+		return nil, fmt.Errorf("could not find GitHub URL: %w", err)
+	}
+	return &commitStatus{
+		owner:  owner,
+		repo:   repo,
+		commit: commitID,
+		repoStatus: github.RepoStatus{
+			Context:     github.String("CodeBuild/example"),
+			State:       github.String(determineCommitStatus(e)),
+			Description: github.String(string(e.Detail.BuildStatus)),
+		},
+	}, nil
+}
+
+func parseGitHubURL(l string) (string, string, error) {
+	if !strings.HasPrefix(l, "https://github.com/") {
+		return "", "", fmt.Errorf("not GitHub URL `%s`", l)
+	}
+	pair := strings.TrimSuffix(strings.TrimPrefix(l, "https://github.com/"), ".git")
+	e := strings.SplitN(pair, "/", 2)
+	if len(e) < 2 {
+		return "", "", fmt.Errorf("could not find owner/repo from `%s`", l)
+	}
+	return e[0], e[1], nil
 }
 
 func determineCommitStatus(e events.CodeBuildEvent) string {
